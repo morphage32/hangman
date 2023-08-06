5d50b50055e9924ce70b1644af8eaccedd1e1a14
morphage32 <mwigand32@yahoo.com>
2023-08-05 18:00:30 -0700

    'Save game' feature has been added.
    The player can now choose to save the game before any guess and can resume the game exactly where it was left off. This feature uses the savegame.txt file in order to work.

--- hangman.rb
+++ hangman.rb
@@ -1,13 +1,24 @@
 class Puzzle
   attr_reader :strikes
+  attr_accessor :seed, :previous_letters
 
   def initialize()
-    wordlist = File.open("google-10000-english-no-swears.txt", "r")
-    @seed = rand(1..10000)
     @previous_letters = []
     @strikes = 0
+    @seed
+    @secret_word
+    @puzzle_letters
+  end
+
+  def save_game()
+    File.write("savegame.txt", "#{@seed}\n#{previous_letters.join}")
+    puts "See you next time!"
+  end
+
+  def get_secret_word()
+    wordlist = File.open("google-10000-english-no-swears.txt", "r")
     line_number = 1
-    
+
     wordlist.each do |line|
       if line_number == @seed
         @secret_word = line.chomp
@@ -137,6 +148,40 @@ def main_game()
   current_puzzle = Puzzle.new()
   guy = Hangman.new()
   victory = false
+  response = 'X'
+
+  unless File.empty?("savegame.txt")
+    until response == 'Y' || response == 'N' do
+      puts "You have a saved game, would you like to resume? ('Y' for yes, 'N' for no): "
+      response = gets.chomp.upcase
+    end
+  end
+
+  if response == 'Y'
+    line_number = 1
+    gamedata = File.open("savegame.txt", "r")
+    gamedata.each do |line|
+      case line_number
+      when 1
+        current_puzzle.seed = line.chomp.to_i
+      when 2
+        current_puzzle.previous_letters = line.chomp.split("")
+      end
+      line_number += 1
+    end
+
+    gamedata.close
+    current_puzzle.get_secret_word
+    current_puzzle.previous_letters.each do |letter|
+      if current_puzzle.check_guess(letter) == 0
+        guy.add_strike(current_puzzle.strikes)
+      end
+    end
+
+  else
+    current_puzzle.seed = rand(1..10000)
+    current_puzzle.get_secret_word
+  end
 
   while current_puzzle.strikes < 6 && victory == false do
     guy.show_hangman
@@ -148,8 +193,14 @@ def main_game()
 
     until player_guess.length == 1 &&
       (player_guess.ord > 96 && player_guess.ord < 123) do
-        puts "\n\nGuess a letter: "
+        puts "\n\nGuess a letter or type 'save' to save and quit: "
         player_guess = gets.downcase.chomp
+
+        if player_guess == 'save'
+          current_puzzle.save_game
+          return
+        end
+
         if current_puzzle.check_previous(player_guess) == true
           puts "Sorry, '#{player_guess}' has already been guessed. Try again."
           player_guess = ""
@@ -168,6 +219,8 @@ def main_game()
     victory = current_puzzle.endgame_check
 
   end
+
+  File.write("savegame.txt", "")
 
   if victory == true
     puts "WINNER! :)"
